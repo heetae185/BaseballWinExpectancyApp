4fdfd7e31519a25064ce4a70e2786478841000d5
heetae185 <heetae185@naver.com>
2023-02-19 16:03:31 +0900

    Constructed base frame

--- lib/widgets/widget_base/widget_base_frame.dart
+++ lib/widgets/widget_base/widget_base_frame.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/cupertino.dart';
 
+class BaseWidgetFrame extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: Color(0xFF9CC06F)),
+      width: MediaQuery.of(context).size.width * 0.25,
+    );
+  }
+}
